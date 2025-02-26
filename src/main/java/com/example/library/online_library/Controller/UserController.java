b6820e459a9601b7aeead35d99bed0a6e1f4ac89
BobTheWho777 <ruslankirillov82@gmail.com>
2025-02-26 10:08:29 +0300

    Add User Controller

---
+++ src/main/java/com/example/library/online_library/Controller/UserController.java
@@ -0,0 +1,26 @@
+package com.example.library.online_library.Controller;
+
+
+import com.example.library.online_library.Service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("api/user")
+public class UserController {
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/all")
+    public String getAllUser(Model model){
+        model.addAttribute("users", userService.getAllUser());
+        return "index";
+    }
+}
